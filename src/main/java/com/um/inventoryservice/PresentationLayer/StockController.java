172b059e3c2a703207c2ddc820b92977971ffc99
Stacey Jin <jin.stacey@gmail.com>
2022-12-09 21:45:44 -0500

    Resolved merge conflict.

--- src/main/java/com/um/inventoryservice/PresentationLayer/StockController.java
+++ src/main/java/com/um/inventoryservice/PresentationLayer/StockController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping("stocks")
 public class StockController {
